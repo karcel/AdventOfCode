ba37f7a12fc16174a11140cf7e9655acbd3605a7
karcel <klasmarcel@gmail.com>
2021-12-16 22:53:06 +0100

    Day 3 challenge - Add 3 2/2 part - update some methods to avoid duplicating

--- src/main/java/DayThree.java
+++ src/main/java/DayThree.java
@@ -4,23 +4,21 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.nio.file.Files.readAllLines;
 
 public class DayThree
 {
     List<String> measurements;
-    Integer[] positionSums;
 
     public DayThree(String filePath)
     {
         measurements = getMeasurements(filePath);
         assert measurements != null;
-        positionSums = new Integer[measurements.get(0).length()];
-        Arrays.fill(positionSums, 0);
 
         System.out.println("Measurements provided:\n" + measurements);
-        System.out.println("Initial positionSums set to:\n" + Arrays.toString(positionSums));
     }
 
     private List<String> getMeasurements(String filePath)
@@ -33,28 +31,35 @@ public class DayThree
         }
     }
 
-    public void calculatePositionSums()
+    public Integer[] calculatePositionSums(List<String> values)
     {
-        for (String measurement : measurements) {
+        Integer[] positionSums = new Integer[values.get(0).length()];
+        Arrays.fill(positionSums, 0);
+
+        for (String measurement : values) {
             for (int i = 0; i < positionSums.length; i++) {
                 if (measurement.charAt(i) == '1')
                     positionSums[i]++;
             }
         }
         System.out.println("Position sums calculated to:\n" + Arrays.toString(positionSums));
+        return positionSums;
     }
 
     public Pair<Integer, Integer> provideRates()
     {
-        Pair<Integer, Integer> rates = new Pair<>(provideRate(Rate.GAMMA), provideRate(Rate.EPSILON));
+        Integer[] positionSums = calculatePositionSums(measurements);
+
+        Pair<Integer, Integer> rates = new Pair<>(provideRate(PowerConsumptionRate.GAMMA, positionSums),
+                provideRate(PowerConsumptionRate.EPSILON, positionSums));
         System.out.printf("Provided rates are:\nGamma= %s\nEpsilon=%s\n", rates.first(), rates.second());
         return rates;
     }
 
-    private Integer provideRate(Rate rate)
+    private Integer provideRate(PowerConsumptionRate powerConsumptionRate, Integer[] positionSums)
     {
         String valueBits = "";
-        switch (rate) {
+        switch (powerConsumptionRate) {
             case GAMMA:
                 for (Integer pos : positionSums)
                     valueBits = valueBits.concat((pos * 2 > measurements.size()) ? "1" : "0");
@@ -65,24 +70,64 @@ public class DayThree
                 break;
         }
 
-        System.out.println(rate + " raw value is " + valueBits);
+        System.out.println(powerConsumptionRate + " raw value is " + valueBits);
 
         return Integer.parseInt(valueBits, 2);
     }
 
     public Integer getPowerConsumption()
     {
-        calculatePositionSums();
-
         Pair<Integer, Integer> rates = provideRates();
 
         return rates.first() * rates.second();
     }
 
+    public Integer getLifeSupportRating()
+    {
+        Integer oxygenRating = calculateOxygenGeneratorRating();
+        Integer co2ScrubberRating = calculateCO2ScrubberRating();
+        System.out.println("Oxygen: " + oxygenRating);
+        System.out.println("CO2: " + co2ScrubberRating);
+        return oxygenRating * co2ScrubberRating;
+    }
 
-    private enum Rate
+    public Integer calculateOxygenGeneratorRating()
+    {
+        return calculateLifeSupportRate(LifeSupportRate.OXYGEN);
+    }
+
+    public Integer calculateCO2ScrubberRating()
+    {
+        return calculateLifeSupportRate(LifeSupportRate.CO2);
+    }
+
+    public Integer calculateLifeSupportRate(LifeSupportRate lifeSupportRate)
+    {
+        System.out.println("Calculating life support rate for " + lifeSupportRate);
+        return Integer.parseInt(
+                findValue(measurements, 0, (lifeSupportRate.equals(LifeSupportRate.OXYGEN))), 2);
+    }
+
+    public String findValue(List<String> measurements, Integer currentBitPosition, boolean mostCommon)
+    {
+        Integer[] positionSums = calculatePositionSums(measurements);
+        char mask = ((positionSums[currentBitPosition] * 2 >= measurements.size()) == mostCommon) ? '1' : '0';
+        List<String> extractedValues = measurements.stream()
+                .filter(x -> x.charAt(currentBitPosition) == mask).collect(Collectors.toList());
+        if (extractedValues.size() > 1)
+            return findValue(extractedValues, currentBitPosition + 1, mostCommon);
+        return extractedValues.get(0);
+    }
+
+    private enum PowerConsumptionRate
     {
         GAMMA,
         EPSILON
+    }
+
+    private enum LifeSupportRate
+    {
+        OXYGEN,
+        CO2
     }
 }
--- src/test/java/DayThreeTest.java
+++ src/test/java/DayThreeTest.java
@@ -11,4 +11,12 @@ public class DayThreeTest
         DayThree dayThree = new DayThree(INPUT_DATA_PATH);
         System.out.println("Total power consumption of the submarine is " + dayThree.getPowerConsumption());
     }
+
+    @Test
+    public void dayTwoTestPartTwo()
+    {
+        DayThree dayThree = new DayThree(INPUT_DATA_PATH);
+        System.out.println("Life support rating of the submarine is: " + dayThree.getLifeSupportRating());
+    }
+
 }
